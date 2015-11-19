c87daafc59af8e0794ee5f77d3bcf34d35989538
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-11-18 22:05:18 -0500

    refactor(events): refactored the events example

--- src/07-events.js
+++ src/07-events.js
@@ -14,8 +14,11 @@ var events = require('events');
 var numberEmitter = new events.EventEmitter();
 numberEmitter.on('number', _.compose(print, byConstant));
 
-var lazy = require('lazy.js');
-lazy(numbers)
-  .async(1000)
-  .each(_.bind(numberEmitter.emit, numberEmitter, 'number'));
+var k = 0;
+var ref = setInterval(function () {
+  numberEmitter.emit('number', numbers[k++]);
+  if (k >= numbers.length) {
+    clearInterval(ref);
+  }
+}, 1000);
 // prints 6, 2 14 with 1 second intervals
